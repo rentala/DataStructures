1551c4fbe50eefc7ba28697f7d8aa64df539189e
rentala <anudeep.rentala@gmail.com>
2017-01-07 12:22:41 -0800

    PreOrder InOrder traversals added

--- DataStructures/src/trees/TestClass.java
+++ DataStructures/src/trees/TestClass.java
@@ -11,9 +11,9 @@ public class TestClass {
 	{
 		
 		//not binary tree test case
-		NotBinaryTreeTestCase();
-		
-		
+		//NotBinaryTreeTestCase();
+		InOrderTraversal();
+		PreorderTraversal();
 		//
 	}
 	public static Tree setUp()
@@ -27,6 +27,40 @@ public class TestClass {
 		children[0].children[2] = new Node(10);
 		children[1] = new Node(3);
 		root.children = children ;
+		/*
+		 * 			1
+		 * 		2		3
+		 * 12 15 10
+		 */
+		return new Tree(root);
+	}
+	public static Tree setUpBST()
+	{
+		Node root = new Node(8);
+		Node[] children = new Node[2];
+		
+		children[0] = new Node(5);
+		children[0].children = new Node[2];
+		children[0].children[0] = new Node(2);
+		children[0].children[1] = new Node(12);
+
+		children[1] = new Node(10);
+		children[1].children = new Node[2];
+		children[1].children[0] = new Node(9);
+		children[1].children[1] = new Node(20);
+
+		root.children = children ;
+		/*
+		 * 			8
+		 * 		5		   10
+		 * 2		12  9		20
+		 */
+		System.out.println("------------------------------");
+		System.out.println("BST Tree: ");
+		System.out.println("	   	8");
+		System.out.println("	5	    10");
+		System.out.println(" 2	   12    9	  20");
+		System.out.println("------------------------------");
 		return new Tree(root);
 	}
 	public static void NotBinaryTreeTestCase()
@@ -38,7 +72,17 @@ public class TestClass {
 
 	public static void PreorderTraversal()
 	{
-		Tree tree = setUp();
+		Tree tree = setUpBST();
+		System.out.print("PreOrder Traversal: ");
+		tree.preOrderTraversal();
+		System.out.println();
 		
+	}
+	public static void InOrderTraversal()
+	{
+		Tree tree = setUpBST();
+		System.out.print("InOrder Traversal: ");
+		tree.inOrderTraversal();
+		System.out.println();
 	}
 }
--- DataStructures/src/trees/Tree.java
+++ DataStructures/src/trees/Tree.java
@@ -13,8 +13,43 @@ public class Tree {
 	{
 		return treeType.evaluate(this);
 	}
+	//print visited node first left to right - but parent before child
 	public void preOrderTraversal()
 	{
-		
+		Node node = this.root;
+		POT(node);
+	}
+	public void POT(Node node)
+	{
+		System.out.print(node.data + " ");
+		if(node.children!= null && node.children.length>0)
+		{
+			for(Node child : node.children)
+			{
+				POT(child);
+				//System.out.println(child.data);
+			}
+			
+		}
+	}
+	//print nodes first left to right - ascending order for BST
+	public void inOrderTraversal()
+	{
+		Node node = this.root;
+		IOT(node);
+	}
+	public void IOT(Node node)
+	{
+		if(node.children!= null && node.children.length>0)
+		{
+			IOT(node.children[0]);
+			
+		}
+		System.out.print(node.data + " ");
+		if(node.children!= null && node.children.length>0)
+		{
+			IOT(node.children[1]);
+			
+		}
 	}
 }
